a50356bf0b44c10932f5e5632e65d553038364cf
mustafaersoyer <mustafaersoyer@gmail.com>
2022-06-21 21:12:25 +0300

    mail change

--- src/components/footer/index.tsx
+++ src/components/footer/index.tsx
@@ -26,12 +26,7 @@ const Footer = () => (
         {' '}
         <div className="flex flex-col">
           <p>If you have a question or anything, feel free to email me:</p>
-          <a
-            href="mailto:
-      info@autorepl.com"
-          >
-            info@autorepl.com
-          </a>
+          <a href="mailto:1mustafaersoy@gmail.com">1mustafaersoy@gmail.com</a>
         </div>
       </div>
       <div className={tw(`w-full lg:w-1/2 mt-12 lg:mt-0`)}>
